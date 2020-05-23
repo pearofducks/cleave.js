9d6d3e72a068bf2ac0027bfc89dc367e8a7fd7f7
Dave Honneffer <pearofducks@gmail.com>
2020-05-23 18:37:49 +0200

    Repackage and cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
-const commonjs = require('rollup-plugin-commonjs');
-const {terser} = require('rollup-plugin-terser');
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: './src/Cleave.js',
   plugins: [commonjs(), terser()],
-  output: {
-    file: './dist/cleave-esm.js',
-    format: 'esm',
-    compact: true
-  }
+  output: [
+    { file: './dist/cleave.esm.js', format: 'esm', compact: true },
+    { file: './dist/cleave.cjs.js', format: 'cjs', compact: true }
+  ]
 }
